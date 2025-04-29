6e4b07b311caa6da1e62779556163955eb1af64e
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2025-04-29 14:33:54 +0530

    testing playwright working

--- playwright_test.py
+++ playwright_test.py
@@ -2,10 +2,16 @@ from playwright.sync_api import sync_playwright
 
 def run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)  # open with GUI
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://google.com")
+
+        query = "apple"
+        page.goto(f"https://duckduckgo.com/?q={query}")
+
+        page.wait_for_timeout(3000)
         print(page.title())
+
+        input("Press Enter to close the browser...")  
         browser.close()
 
 if __name__ == "__main__":
